f4aab35602b7c3611b37399aca5a21dbf78e823d
EdwinAvila <238208705+WinchoCode@users.noreply.github.com>
2026-06-09 15:29:50 +0000

    first practice with vectors

--- src/Ch04/04_06b/CodeDemo.cpp
+++ src/Ch04/04_06b/CodeDemo.cpp
@@ -2,11 +2,24 @@
 // Exercise 04_06
 // The Vector Class, by Eduardo Corpeño 
 
+#include <vector>
 #include <iostream>
 #include <string>
 
 int main(){
-    
+    std::vector<std::string> checkpoint = {"Start", "Forest", "Castle"};
+
+    checkpoint.push_back("Cave");
+    checkpoint.push_back("Finish");
+
+    std::cout << "The game has: " << checkpoint.size() << " checkpoints." << std::endl;
+
+    std::cout << "The checkpoint at the index 2 is: " << checkpoint[2] << std::endl;
+
+    checkpoint[2] = "Dark Castle";
+    std::cout << "The new checkpoint at the index 2 is: " << checkpoint[2] << std::endl;
+
+
     std::cout << std::endl << std::endl;
     return 0;
 }
